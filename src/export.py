bac0f6bc6abd378cdd1d9019ce4abd0da7476c0f
Andrew Mihal <andrewcmihal@gmail.com>
2025-03-31 19:05:27 -0700

    Removed timers for debugging cnf export performance

--- src/export.py
+++ src/export.py
@@ -1,5 +1,4 @@
 import sys
-import time
 from z3 import Goal, is_not, Not, Tactic, Then, With, Z3_OP_UNINTERPRETED
 
 def print_formula(formula, indent=0, recursive=True):
@@ -34,23 +33,13 @@ def export_cnf(formulation, solver, filename):
     goal.add(solver.assertions())
 
     # Apply a set of tactics to convert a purely CNF formulation.
-    print()
-    print(f'Applying tactics to convert formulation to CNF.', flush=True)
-    tactic_start_time = time.process_time()
     tactic = Then('lia2card', 'dt2bv', 'bit-blast', 'card2bv', 'tseitin-cnf')
     subgoal = tactic(goal)
     formulas = subgoal[0]
-    tactic_end_time = time.process_time()
-    tactic_elapsed_time = tactic_end_time - tactic_start_time
-    print(f'Tactics generated {len(formulas)} formulas in {tactic_elapsed_time:.2f} seconds.', flush=True)
 
     # Collect all variables in all formulas.
-    print()
-    print(f'Starting CNF variable enumeration.', flush=True)
-    var_enum_start_time = time.process_time()
     all_vars = set()
     for f in formulas:
-        #print_formula(f)
         num_args = f.num_args()
         if num_args == 0:
             # Formula is a single positive literal.
@@ -69,14 +58,8 @@ def export_cnf(formulation, solver, filename):
                 else:
                     # Negative literal
                     all_vars.add(arg.arg(0).decl().name())
-    var_enum_end_time = time.process_time()
-    var_enum_elapsed_time = var_enum_end_time - var_enum_start_time
-    print(f'CNF variable enumeration finished in {var_enum_elapsed_time:.2f} seconds.', flush=True)
 
     # Split all the CNF vars into the placement/nonplacement groups.
-    print()
-    print(f'Splitting placement/non-placement CNF variables.', flush=True)
-    var_split_start_time = time.process_time()
     placement_vars = []
     nonplacement_vars = []
     for var in all_vars:
@@ -84,26 +67,11 @@ def export_cnf(formulation, solver, filename):
             placement_vars.append(var)
         else:
             nonplacement_vars.append(var)
-    var_split_end_time = time.process_time()
-    var_split_elapsed_time = var_split_end_time - var_split_start_time
-    print(f'CNF variable splitting finished in {var_split_elapsed_time:.2f} seconds.', flush=True)
 
-    print()
-    print(f'Assigning indices to CNF variables.', flush=True)
-    index_assign_start_time = time.process_time()
-
-    # Map placement vars to integers starting at 1.
-    # Nonplacement vars are numbered after the placement vars.
+    # Map sorted placement vars to integers starting at 1.
+    # Nonplacement vars are numbered after the placement vars and don't need to be sorted.
     # Enumerating the variables this way allows import to work even if the tactic or finite-domain sort changes.
     var_to_ix_map = {var:ix for ix, var in enumerate(sorted(placement_vars) + nonplacement_vars, start=1)}
-
-    index_assign_end_time = time.process_time()
-    index_assign_elapsed_time = index_assign_end_time - index_assign_start_time
-    print(f'CNF variable index assignment finished in {index_assign_elapsed_time:.2f} seconds.', flush=True)
-
-    print()
-    print(f'Writing CNF output file.', flush=True)
-    cnf_write_start_time = time.process_time()
 
     with open(filename, 'w', encoding='ascii') as cnf_file:
         # Write cnf header
@@ -132,10 +100,6 @@ def export_cnf(formulation, solver, filename):
 
             cnf_file.write(' '.join(map(str, literals)) + ' 0\n')
 
-    cnf_write_end_time = time.process_time()
-    cnf_write_elapsed_time = cnf_write_end_time - cnf_write_start_time
-    print(f'CNF file write finished in {cnf_write_elapsed_time:.2f} seconds.', flush=True)
-
 def import_certificate(formulation, filename):
     # Sorting all of the placement bvars in the formulation by name should match the numbering used in the CNF export.
     # One additional list element at the beginning is necessary because CNF starts numbering at 1, not 0.
@@ -147,8 +111,7 @@ def import_certificate(formulation, filename):
         for line in certificate:
             if line.startswith("v "):
                 var_list = line.rstrip().split()
-                int_list = [int(var) for var in var_list[1:]]
-                for assignment in int_list:
+                for assignment in map(int, var_list[1:]):
                     if 0 < assignment and assignment < len(sorted_placement_bvars):
                         assertions.append(sorted_placement_bvars[assignment])
                     elif 0 < -assignment and -assignment < len(sorted_placement_bvars):
