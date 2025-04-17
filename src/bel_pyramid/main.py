dcaafae3239d4252039a5d9a1841bbe00b707533
Andrew Mihal <andrewcmihal@gmail.com>
2025-04-17 16:20:05 -0700

    Use relative imports intra-package

--- src/bel_pyramid/main.py
+++ src/bel_pyramid/main.py
@@ -1,7 +1,7 @@
 import argparse
-from bel_pyramid.export import export_cnf, export_smt2, import_certificate
-from bel_pyramid.formulation import Formulation
-from bel_pyramid.strategies import StrategyMap
+from .export import export_cnf, export_smt2, import_certificate
+from .formulation import Formulation
+from .strategies import StrategyMap
 import time
 from z3 import Solver, Tactic, sat
 
