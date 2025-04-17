226d77db80ab61b5d18de7fb8f019b5114b6ba52
Andrew Mihal <andrewcmihal@gmail.com>
2025-04-17 16:35:16 -0700

    Collect kissat process times

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 ls benchmarks/*.cnf.gz | parallel kissat {} ">" {.}.cert
+
+# Sort results by Kissat process-time.
+grep -H process-time benchmarks/*.cert | awk '{print $1, $(NF-1)}' | sort -n -k 2 | tee benchmarks/sorted_runtimes.txt
